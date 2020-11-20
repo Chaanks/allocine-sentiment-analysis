1406429a6f4f3b5f24d16a7a6b722892100e41a0
chaanks <jarod8405@gmail.com>
2020-11-20 18:58:05 +0100

    add curl cmd to create the new index

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-for file in $1/*.ndjson; do
+curl -X PUT "localhost:9200/movie_db" -H 'Content-Type: application/json' -d "@$1"
+
+for file in $2/*.ndjson; do
     echo "upload $file"
     curl -o /dev/null -H 'Content-Type: application/x-ndjson' -XPOST 'localhost:9200/movie_db/_bulk?pretty' --data-binary @$file
 done
